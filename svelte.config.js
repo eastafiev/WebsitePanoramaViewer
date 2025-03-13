dba588476fbcaedc50cb95d4712375304b168fe5
Eugene Astafiev <iasta@softserveinc.com>
2025-03-13 22:17:04 +0200

    Change adapter from Cloudflare to Static and update configuration options

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import remarkA11yEmoji from "@fec/remark-a11y-emoji";
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-static"; // Changed from adapter-cloudflare
 import { mdsvex } from "mdsvex";
 import rehypeColorPreview from "rehype-color-preview";
 import rehypeSlug from "rehype-slug";
@@ -16,7 +16,19 @@ const config = {
 			$layout: "src/layout",
 			$i18n: "src/i18n",
 		},
-		adapter: adapter(),
+		adapter: adapter({
+			// default options are shown
+			pages: "build",
+			assets: "build",
+			fallback: "index.html",
+			precompress: false,
+			strict: true,
+		}),
+		paths: {
+			// Update this to match your GitHub repository name
+			base:
+				process.env.NODE_ENV === "production" ? "/SimplePanoViewerWebSite" : "",
+		},
 	},
 	preprocess: [
 		mdsvex({
